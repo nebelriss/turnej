e59a81044c93fcb729c64d22e5ff95e4d36b106b
Michel Heiniger <git@heiniger.io>
2021-03-25 21:22:15 +0100

    added mailer

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
